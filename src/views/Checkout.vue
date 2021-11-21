e957790b779f115a2de76dc632f83d4f9749f85a
huanmingchang <huanming1988@gmail.com>
2021-11-21 11:25:58 +0800

    feat: add stepper component and main title

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,15 +1,19 @@
 <template>
-  <main>
+  <main class="main">
+    <div class="main__title">結帳</div>
+    <Stepper />
     <ShoppingCart />
   </main>
 </template>
 
 <script>
+import Stepper from '../components/Stepper'
 import ShoppingCart from '../components/ShoppingCart'
 export default {
   name: 'Cart',
   components: {
     ShoppingCart,
+    Stepper,
   },
 }
 </script>
@@ -21,10 +25,41 @@ export default {
 @import '../styles/layout.scss';
 @import '../styles/size.scss';
 
+.main {
+  margin: 0 15px;
+}
+
+.main__title {
+  margin: 1rem 0 1.5rem;
+  font-size: 1.5rem;
+  font-weight: 700;
+  line-height: 38px;
+}
+
 @media (min-width: 576px) {
   .main {
     width: 80%;
     margin: 0 auto;
+  }
+}
+@media (min-width: 768px) {
+  .main__title {
+    margin: 5rem 0 3rem;
+    font-size: 2rem;
+  }
+}
+
+@media (min-width: 1200px) {
+  .main {
+    width: 80%;
+    margin: 0 auto;
+    padding: 0;
+  }
+}
+
+@media (min-width: 1400px) {
+  .main {
+    max-width: 1440px;
   }
 }
 </style>
