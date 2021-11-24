27c22ac42d451e352345e5f48b15e44ea4e764c0
huanmingchang <huanming1988@gmail.com>
2021-11-25 07:21:50 +0800

    feat: add button control form panel function

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -11,8 +11,17 @@
     </div>
     <ShoppingCart />
     <div class="main__button-panel">
-      <button class="main__button-panel__btn btn-outline d-none">上一步</button>
-      <button class="main__button-panel__btn btn-primary first-step">
+      <button
+        v-show="currentStep !== 1"
+        class="main__button-panel__btn btn-outline"
+        @click.prevent.stop="GoPrevStep"
+      >
+        上一步
+      </button>
+      <button
+        class="main__button-panel__btn btn-primary first-step"
+        @click.prevent.stop="GoNextStep"
+      >
         下一步
       </button>
     </div>
@@ -28,6 +37,49 @@ export default {
   components: {
     Stepper,
     ShoppingCart,
+  },
+  data() {
+    return {
+      currentStep: 1,
+    }
+  },
+  methods: {
+    GoNextStep() {
+      const nextBtn = document.querySelector('.btn-primary')
+      if (this.currentStep === 1) {
+        this.$router.push({ name: 'second-step' })
+        nextBtn.classList.remove('first-step')
+      }
+
+      if (this.currentStep === 2) {
+        this.$router.push({ name: 'third-step' })
+        nextBtn.innerText = '確認下單'
+        nextBtn.classList.add('last-step')
+      }
+      this.currentStep++
+
+      if (this.currentStep >= 3) {
+        this.currentStep = 3
+      }
+    },
+    GoPrevStep() {
+      const nextBtn = document.querySelector('.btn-primary')
+      if (this.currentStep === 3) {
+        this.$router.push({ name: 'second-step' })
+        nextBtn.innerText = '下一步'
+        nextBtn.classList.remove('last-step')
+      }
+
+      if (this.currentStep === 2) {
+        this.$router.push({ name: 'first-step' })
+        nextBtn.classList.add('first-step')
+      }
+      this.currentStep--
+
+      if (this.currentStep <= 1) {
+        this.currentStep = 1
+      }
+    },
   },
 }
 </script>
